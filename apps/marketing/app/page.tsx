6d3bb7c1aa8c011f008100ba0f277efdc0a9e4f4
yop-dev <desilvajoner99@gmail.com>
2026-02-24 14:05:09 +0800

    feat(marketing): wire WorkflowVideoSection into page

--- apps/marketing/app/page.tsx
+++ apps/marketing/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/sections/HeroSection";
 import PainPointSection from "@/components/sections/PainPointSection";
 import ProductShowcaseSection from "@/components/sections/ProductShowcaseSection";
 import FeaturesSection from "@/components/sections/FeaturesSection";
+import WorkflowVideoSection from "@/components/sections/WorkflowVideoSection";
 import BenefitsSection from "@/components/sections/BenefitsSection";
 import IndustriesSection from "@/components/sections/IndustriesSection";
 import ShowcaseSection from "@/components/sections/ShowcaseSection";
@@ -17,6 +18,7 @@ export default function Home() {
                 <PainPointSection /> {/* Moved to #2 */}
 
                 <FeaturesSection />
+                <WorkflowVideoSection />
                 <ProductShowcaseSection />
                 <BenefitsSection />
                 <IndustriesSection />
