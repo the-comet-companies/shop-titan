88d95ce7dd6963b24050ff775781a12b9731a266
yop-dev <desilvajoner99@gmail.com>
2026-02-24 13:59:32 +0800

    feat(remotion): assemble WorkflowVideo composition with Series

---
+++ apps/remotion/src/WorkflowVideo/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { AbsoluteFill, Series } from "remotion";
+import { Intro } from "./Intro";
+import { Outro } from "./Outro";
+import { Stage } from "./Stage";
+import { STAGES, STAGE_FRAMES, INTRO_FRAMES, OUTRO_FRAMES } from "./data";
+
+export const WorkflowVideo: React.FC = () => {
+  return (
+    <AbsoluteFill style={{ backgroundColor: "#0A0A0A" }}>
+      <Series>
+        <Series.Sequence durationInFrames={INTRO_FRAMES} premountFor={30}>
+          <Intro />
+        </Series.Sequence>
+
+        {STAGES.map((stage) => (
+          <Series.Sequence
+            key={stage.id}
+            durationInFrames={STAGE_FRAMES}
+            premountFor={15}
+          >
+            <Stage stage={stage} />
+          </Series.Sequence>
+        ))}
+
+        <Series.Sequence durationInFrames={OUTRO_FRAMES} premountFor={15}>
+          <Outro />
+        </Series.Sequence>
+      </Series>
+    </AbsoluteFill>
+  );
+};
